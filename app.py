5ffb6f0c8268011cce48c7ac21a808bd95e036a1
miguellperezzv <42900928+miguellperezzv@users.noreply.github.com>
2022-06-05 16:28:16 -0500

    Global colors

--- app.py
+++ app.py
@@ -5,17 +5,24 @@ from dash import html
 #import dash_html_components as html
 
 app = dash.Dash()
+
+colors = {
+    'text' : "#ff0000",
+    'plot_color' : "#D3D3D3",
+    'papel_color' : "#D3D3D3",
+}
+
 app.layout = html.Div([
     html.H1(children = "Hello Dash world",
         style = {
             'textAlign' : "center",
-            'color' : "#456FBV"
+            'color' : colors["text"]
         }
     ),
     html.Div(children = "Dash !!!!!!!!!!!- product!",
         style = {
             'textAlign' : "center",
-            'color' : "#456FBV"
+            'color' : colors["plot_color"]
         }
     
     ),
@@ -28,6 +35,11 @@ app.layout = html.Div([
                 {'x' : [5,6,7], 'y': [2,12,10], 'type' : 'bar', 'name': 'First Chart'}
             ],
             'layout': {
+                'plot_bgcolor' : colors["plot_color"],
+                'paper_bgcolor': "#D3D3D3",
+                'font' : {
+                    'color' : "ff0000",
+                },
                 'title' : "Simple Bar Chart"
             }
         }
