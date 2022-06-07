4cb74523156847070918fc45c80233483fbdeb90
miguellperezzv <42900928+miguellperezzv@users.noreply.github.com>
2022-06-07 10:10:12 -0500

    Colores procolombia

--- app/style/style.py
+++ app/style/style.py
@@ -1,15 +1,29 @@
+#global colors
+PROCOLOMBIA_COLORS = {
+    'yellow' : "#FFBA00",
+    'blue' : "#007DCA", 
+    'red' : "#F00030",
+
+    'clear_yellow' : '#FFD977',
+    'clear_blue'   : '#95BDCC',
+    'clear_red'     : '#F0849D'
+}
+
+
+
 # the style arguments for the sidebar.
 SIDEBAR_STYLE = {
-    'position': 'fixed',
+    #'position': 'fixed',
     'top': 0,
     'left': 0,
     'bottom': 0,
     'width': '20%',
     'padding': '20px 10px',
-    'background-color': '#f8f9fa',
+    'background-color': PROCOLOMBIA_COLORS['clear_blue'],
     'margin-top':'100px'
 
 }
+print(PROCOLOMBIA_COLORS['yellow'])
 
 # the style arguments for the main content page.
 CONTENT_STYLE = {
@@ -28,3 +42,4 @@ CARD_TEXT_STYLE = {
     'textAlign': 'center',
     'color': '#0074D9'
 }
+
