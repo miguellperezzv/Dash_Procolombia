9785897df26da8465c438fcc47a44cc5016f82ce
miguellperezzv <42900928+miguellperezzv@users.noreply.github.com>
2022-06-06 20:00:54 -0500

    dash

--- app.py
+++ app.py
@@ -137,6 +137,8 @@ sidebar = html.Div(
     style=SIDEBAR_STYLE,
 )
 
+
+
 content_first_row = dbc.Row([
     dbc.Col(
         dbc.Card(
@@ -244,8 +246,40 @@ content = html.Div(
     style=CONTENT_STYLE
 )
 
+
+
+
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.layout = html.Div([sidebar, content])
+
+@app.callback(
+    Output('card_title_1', 'children'),
+    [Input('submit_button', 'n_clicks')],
+    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
+     State('radio_items', 'value')
+     ])
+def update_card_title_1(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
+    print(n_clicks)
+    print(dropdown_value)
+    print(range_slider_value)
+    print(check_list_value)
+    print(radio_items_value)  # Sample data and figure
+    return 'Card Tile 1 change by call back'
+
+
+@app.callback(
+    Output('card_text_1', 'children'),
+    [Input('submit_button', 'n_clicks')],
+    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
+     State('radio_items', 'value')
+     ])
+def update_card_text_1(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
+    print(n_clicks)
+    print(dropdown_value)
+    print(range_slider_value)
+    print(check_list_value)
+    print(radio_items_value)  # Sample data and figure
+    return 'Card text change by call back'
 
 if __name__ == '__main__':
     app.run_server(port= 5050, debug=True)
