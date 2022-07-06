ce4bf43fe62319e0ddc055e900df881b15bbd00f
miguellperezzv <42900928+miguellperezzv@users.noreply.github.com>
2022-07-05 19:20:02 -0500

    ejecutar servidor

--- app.py
+++ app.py
@@ -58,4 +58,6 @@ def render_content(tab):
         return region.content
 
 if __name__ == '__main__':
-    app.run_server(port= 5050, debug=True)
+    #app.run_server(port= 5050, debug=True)
+    
+    app.run_server(host='0.0.0.0', port= 9000, debug=True)
