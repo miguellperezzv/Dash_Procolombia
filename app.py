81ad7bafd060ef030471952c93d97e797fbf0759
miguellperezzv <42900928+miguellperezzv@users.noreply.github.com>
2022-07-07 02:15:39 -0500

    pestaña load

--- app.py
+++ app.py
@@ -1,35 +1,29 @@
 import dash
+import os
 import dash_bootstrap_components as dbc
-from dash import dcc
-from dash import html
-from dash.dependencies import Input, Output, State
-from app import sidebar
-from components import content, destacados, navbar, region, insights
-from logica import  controlador
-import plotly.express as px
-from assets import style
-import numpy as np
+from dash import dcc, html
+from dash.dependencies import Input, Output
 
-
+from components import content, destacados, region, navbar, insights
+import dash_uploader as du
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
 app.config['suppress_callback_exceptions'] = True
-
-
 
 app.layout = html.Div([
 
     navbar.navbar,
     html.Br(),
     dbc.Col([
-        dcc.Tabs(id="tabs", value='tab_option', children=[
-        
+        dcc.Tabs(id="tabs", value='tab_load', children=[
+           
+
         dcc.Tab(label='Visualización por Hubs', value='tab_hubs'),
         dcc.Tab(label='Visualización por países', value='tab_paises'),
         dcc.Tab(label='Países destacados', value='tab_destacados'),
-        dcc.Tab(label='Load', value='tab_load'),
-        dcc.Tab(label="About", value = 'tab_insights' )
-        
+        dcc.Tab(label='Dataset', value='tab_load'),
+        dcc.Tab(label='Entrenamiento', value=''),     
+        dcc.Tab(label="About", value = 'tab_insights')
 
     ]),
     ]),
@@ -38,14 +32,7 @@ app.layout = html.Div([
     ]),
     #content.content,
 
-    
-
 ], className="principalDiv")
-
-
-
-
-
 
 @app.callback(Output('tab_general', 'children'),
               Input('tabs', 'value'))
@@ -60,8 +47,50 @@ def render_content(tab):
         return region.content
     elif tab == "tab_insights":
         return insights.content
+    else:
+        return layout
+        
+UPLOAD_FOLDER_ROOT = os.path.dirname(__file__)+r"/modelos"
+
+du.configure_upload(app, UPLOAD_FOLDER_ROOT,use_upload_id=False)
+
+def get_upload_component(id):
+    return du.Upload(
+        id=id,
+        max_file_size=1800,  # 1800 Mb
+        max_files=100
+    )
+
+layout = html.Div(
+        [
+            html.H1('Joblibs'),
+            
+            html.Div(
+                [
+                    html.H4('Decargar el archivo para generar los joblist'),
+                    dcc.Link('Descarga', href='assets/entrenar.py', target='_blank'),
+                    get_upload_component(id='dash-uploader'),
+                    html.Div(id='callback-output'),
+                ],
+                style={  # wrapper div style
+                    'textAlign': 'center',
+                    'width': '600px',
+                    'padding': '10px',
+                    'display': 'inline-block'
+                }),
+        ],
+        style={
+            'textAlign': 'center',
+        },
+    )
+
+@du.callback(
+    output=Output("callback-output", "children"),
+    id="dash-uploader",
+)
+def callback_on_completion(status: du.UploadStatus):
+    return html.Ul([html.Li(str(x)) for x in status.uploaded_files])            
+
 
 if __name__ == '__main__':
-    #app.run_server(port= 5050, debug=True)
-    
-    app.run_server(host='0.0.0.0', port= 9000, debug=True)
+    app.run_server(host='0.0.0.0', port= 7000, debug=True)
--- components/insights.py
+++ components/insights.py
@@ -5,5 +5,154 @@ from dash.dependencies import Input, Output, State
 from components import summary
 
 content = html.Div([
-    "Hola Mundo"
-])
+
+    html.Hr(),
+    dbc.Row([
+        dbc.Col([
+            dbc.Card(
+    [
+        dbc.Row(
+            [
+                dbc.Col(
+                    dbc.CardImg( 
+                        src="assets\\team_76.JPG",
+                        className="img-fluid rounded-start",
+                    ),
+                    className="col-md-4",
+                ),
+                dbc.Col(
+                    dbc.CardBody(
+                        [
+                            html.H4("TEAM 76", className="card-title"),
+                            html.P(
+                                "This project was made by Team 76  "
+                                "below as a natural lead-in to additional "
+                                "content. This content is a bit longer.",
+                                className="card-text",
+                            ),
+                            html.Small(
+                                
+                                className="card-text text-muted",
+                            ),
+                        ]
+                    ),
+                    className="col-md-8",
+                ),
+            ],
+            className="g-0 d-flex align-items-center",
+        )
+    ],
+    className="mb-3",
+    style={"maxWidth": "540px"},
+)
+        ]),
+
+
+
+    dbc.Col([
+            dbc.Card(
+    [
+        dbc.Row(
+            [
+                dbc.Col(
+                    dbc.CardImg( 
+                        src="assets\\C1.webp",
+                        className="img-fluid rounded-start",
+                    ),
+                    className="col-md-4",
+                ),
+                dbc.Col(
+                    dbc.CardBody(
+                        [
+                            html.H4("Correlation One", className="card-title"),
+                            html.P(
+                                "During the development of the Data Science course - Correlation One (Cohort 6 - 2022).",
+                                className="card-text",
+                            ),
+                            html.Small(
+                                
+                                className="card-text text-muted",
+                            ),
+                        ]
+                    ),
+                    className="col-md-8",
+                ),
+            ],
+            className="g-0 d-flex align-items-center",
+        )
+    ],
+    className="mb-3",
+    style={"maxWidth": "540px"},
+)
+        ]),
+    
+
+    dbc.Col([
+            dbc.Card(
+    [
+        dbc.Row(
+            [
+                dbc.Col(
+                    dbc.CardImg( 
+                        src="assets\\procolombia.png",
+                        className="img-fluid rounded-start",
+                    ),
+                    className="col-md-4",
+                ),
+                dbc.Col(
+                    dbc.CardBody(
+                        [
+                            html.H4("Procolombia", className="card-title"),
+                            html.P(
+                                "The objective of this project is to apply the knowledge learned in the course, developing a methodology to identify and predict the impact that each of its promotional activities in different countries have on the arrival of tourists to Colombia",
+                                className="card-text",
+                            ),
+                            html.Small(
+                                
+                                className="card-text text-muted",
+                            ),
+                        ]
+                    ),
+                    className="col-md-8",
+                ),
+            ],
+            className="g-0 d-flex align-items-center",
+        )
+    ],
+    className="mb-3",
+    style={"maxWidth": "540px"},
+)
+        ])
+    
+    ]),
+    html.Hr(),
+    dbc.Row([
+        html.P("This application has 3 sections:"),
+        html.Li("Load: Here you must attach the necessary files for loading, exploration, cleaning, identification of correlations and training of the model, this must be provided by ProColombia. It should be considered to keep updated the databases referring to control variables such as currency information, IPC, fairs and carnivals."),
+        html.Li("Visualization: Here you can see the information according to the region and the country that is selected, graphs with the numbers of tourists predicted in different periods. Also, the impact of carrying out the campaigns in the country can be observed."),
+        html.Li("Featured countries: Relevant countries for Procolombia")
+    ]),
+    html.Hr(),
+    dbc.Row([
+        html.P("To take into account, a convention is used for the appointment of promotional activities:"),
+        html.Br(),
+        html.Li("x1: Agenda comercial de turismo"),
+        html.Br(),
+        html.Li("x2: Agendas de cooperación / misiones"),
+        html.Br(),
+        html.Li("x3: Capacitaciones y presentaciones de destino"),
+        html.Br(),
+        html.Li("x4: Entrega información valor agregado"),
+        html.Br(),
+        html.Li("x5: Fam - press trips"),
+        html.Br(),
+        html.Li("x6: Feria internacional de turismo"),
+        html.Br(),
+        html.Li("x7: Macro ruedas y encuentros comerciales"),
+        html.Br(),
+        html.Li("x8: Otras acciones promoción turismo"),
+        html.Br(),
+        html.Li("x9: Primera visita")
+        
+    ])
+],  style = {'margin-left' : "30px"} )
