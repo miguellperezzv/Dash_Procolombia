0ba07ee4c7cc5b558fb38e30cb0315b9680639cd
miguellperezzv <42900928+miguellperezzv@users.noreply.github.com>
2022-07-07 18:31:59 -0500

    Tab de about actualizada

--- components/insights.py
+++ components/insights.py
@@ -13,9 +13,10 @@ content = html.Div([
                 dbc.Col(
                     dbc.CardImg( 
                         src="assets\\team_76.JPG",
+                        
                         className="img-fluid rounded-start",
                     ),
-                    className="col-md-4",
+                    className="col-md-4", style={"margin-left": "auto", "margin-right": "auto"},
                 ),
                 dbc.Col(
                     dbc.CardBody(
@@ -55,6 +56,7 @@ content = html.Div([
                     dbc.CardImg( 
                         src="assets\\C1.webp",
                         className="img-fluid rounded-start",
+                        style={"margin-left": "auto", "margin-right": "auto"}
                     ),
                     className="col-md-4",
                 ),
@@ -93,6 +95,7 @@ content = html.Div([
                     dbc.CardImg( 
                         src="assets\\procolombia.png",
                         className="img-fluid rounded-start",
+                        style={"margin-left": "auto", "margin-right": "auto"}
                     ),
                     className="col-md-4",
                 ),
@@ -131,25 +134,68 @@ content = html.Div([
     ]),
     html.Hr(),
     dbc.Row([
-        html.P("To take into account, a convention is used for the appointment of promotional activities:"),
-        html.Br(),
-        html.Li("x1: Agenda comercial de turismo"),
-        html.Br(),
-        html.Li("x2: Agendas de cooperación / misiones"),
-        html.Br(),
-        html.Li("x3: Capacitaciones y presentaciones de destino"),
-        html.Br(),
-        html.Li("x4: Entrega información valor agregado"),
-        html.Br(),
-        html.Li("x5: Fam - press trips"),
-        html.Br(),
-        html.Li("x6: Feria internacional de turismo"),
-        html.Br(),
-        html.Li("x7: Macro ruedas y encuentros comerciales"),
-        html.Br(),
-        html.Li("x8: Otras acciones promoción turismo"),
-        html.Br(),
-        html.Li("x9: Primera visita")
+        dbc.Col([
+            html.H2("Standards"),
+            html.Br(),
+            html.P("To take into account, a convention is used for the appointment of promotional activities:"),
+            html.Br(),
+            html.Li("x1: Agenda comercial de turismo."),
+            html.Li("x2: Agendas de cooperación / misiones."),
+            html.Li("x3: Capacitaciones y presentaciones de destino."),
+            html.Li("x4: Entrega información valor agregado."),
+            html.Li("x5: Fam - press trips."),
+            html.Li("x6: Feria internacional de turismo."),
+            html.Li("x7: Macro ruedas y encuentros comerciales."),
+            html.Li("x8: Otras acciones promoción turismo."),
+            html.Li("x9: Primera visita.")
+
+        ], lg=4, md=12),
         
+        dbc.Col([
+            html.H2("The Model", style={"text-align": "center"}),
+            html.Br(),
+            html.P("In order to know the performance of each of the promotional activities, identify at what point in time they have the greatest impact and the long-term prediction of the arrival of tourists to Colombia, two modeling guidelines were designed:"),
+            dbc.Row([
+                dbc.Col([
+                    dbc.Card(
+                    [
+                    dbc.CardImg(src="assets\\prophet.png", top=True, ),
+                    dbc.CardBody([
+                    html.H4("Time series model (Prophet):"),
+                    html.P("A time series model was programmed using the Prophet that allows the user to choose the country and how many periods ahead he wants to see the influence of promotional activities. Initially, 9 models per country are calculated taking into account the lag, where the percentage gap between the model prediction and the observed data is calculated, all this eliminating at least one of the activities of each model, with this exercise it can be said that the Missing value in the prediction comes from missing activity.", className="card-text")
+                    ]),
+                    ],
+                    style={"width": "18rem"},
+)
+                ]),
+                dbc.Col([
+
+                ]),
+
+                dbc.Col([
+                    dbc.Card(
+                    [
+                    dbc.CardImg(src="assets\\xgboost.png", top=True, style={"size": "20rem"}),
+                    dbc.CardBody([
+                    html.H4("Prediction models (Gradient Boosting and Xgboost):"),
+                    html.P("For this model it was necessary to automate the training and the choice of the model in such a way that the user interaction is minimal. Therefore, four types of models with two types of vision were created, that is, four analysis models per axis and four analysis models per country, which were designed as follows: One that trains a gradient-enhancing regressor in search of optimizing the parameters using GridSearchCV and using K-Fold to improve its learning, in addition to searching by period (forward) for the influence of promotional activities on the flow of travelers by hub or city , this will generate as many models as periods forward are taken, so if the user advances five periods, the five models of this class will be obtained. It is important to clarify that the only variables that are carried out are promotional activities.", className="card-text")
+                    ]),
+                    ],
+                    style={"width": "30rem"},
+)
+                ]),
+                dbc.Col([
+
+                ]),
+
+            ])
+        ], lg=8, md=12 )
+
+
+        
+    ]),
+    html.Hr(),
+    dbc.Row([
+
     ])
 ],  style = {'margin-left' : "30px"} )
